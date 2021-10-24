2df6c2c32547d814560a283642eadd52b5f3039a
Aydin Yalçinkaya <aydincodes@gmail.com>
2021-10-24 16:07:36 -0400

    Added JS for main page button.

---
+++ main.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("findParking");
+
+function findParking() {
+    let location = prompt("Where would you like to find parking?");
+    alert("Sorry, we do not have any parking in " + location + ". Please check back later!")
+}
+
